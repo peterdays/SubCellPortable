31d008f8f16047194ad31739f54e090cf6861e83
Ankit Gupta <ankit.gupta@it.uu.se>
2024-10-01 10:42:13 +0200

    fixed model_config.yaml for multiple classifier paths, added gpu support, added faster attention for GPU, fixed standardize, removed dropouts

--- inference.py
+++ inference.py
@@ -1,13 +1,8 @@
-from dataclasses import dataclass
-from typing import Dict, Optional, Tuple
-
 import numpy as np
 import torch
+import torch.nn.functional as F
 from skimage.io import imsave
-from torch import Tensor, nn
-from transformers import ViTConfig, ViTModel
-from transformers.utils import ModelOutput
-
+from torchvision.utils import make_grid
 
 CLASS2NAME = {
     0: "Actin filaments",
@@ -44,32 +39,32 @@ CLASS2NAME = {
 }
 
 CLASS2COLOR = {
-    0: '#ffeb3b',
-    1: '#76ff03',
-    2: '#ff6d00',
-    3: '#eb30c1',
-    4: '#faadd4',
-    5: '#795548',
-    6: '#64ffda',
-    7: '#00e676',
-    8: '#03a9f4',
-    9: '#4caf50',
-    10: '#ffc107',
-    11: '#00bcd4',
-    12: '#cddc39',
-    13: '#212121',
-    14: '#8bc34a',
-    15: '#ff9800',
-    16: '#ae8c08',
-    17: '#ffff00',
-    18: '#31b61f',
-    19: '#9e9e9e',
-    20: '#2196f3',
-    21: '#e91e63',
-    22: '#3f51b5',
-    23: '#9c27b0',
-    24: '#673ab7',
-    25: '#d3a50b',
+    0: "#ffeb3b",
+    1: "#76ff03",
+    2: "#ff6d00",
+    3: "#eb30c1",
+    4: "#faadd4",
+    5: "#795548",
+    6: "#64ffda",
+    7: "#00e676",
+    8: "#03a9f4",
+    9: "#4caf50",
+    10: "#ffc107",
+    11: "#00bcd4",
+    12: "#cddc39",
+    13: "#212121",
+    14: "#8bc34a",
+    15: "#ff9800",
+    16: "#ae8c08",
+    17: "#ffff00",
+    18: "#31b61f",
+    19: "#9e9e9e",
+    20: "#2196f3",
+    21: "#e91e63",
+    22: "#3f51b5",
+    23: "#9c27b0",
+    24: "#673ab7",
+    25: "#d3a50b",
     26: "#f44336",
     27: "#009688",
     28: "#ff9e80",
@@ -78,156 +73,41 @@ CLASS2COLOR = {
 }
 
 
-def min_max_standardize(im, min_perc=0, max_perc=100):
-    min_val = np.percentile(im, min_perc, axis=(1, 2, 3), keepdims=True)
-    max_val = np.percentile(im, max_perc, axis=(1, 2, 3), keepdims=True)
+def min_max_standardize(im):
+    min_val = torch.min(im, dim=(1, 2, 3), keepdims=True)[0]
+    max_val = torch.max(im, dim=(1, 2, 3), keepdims=True)[0]
 
     im = (im - min_val) / (max_val - min_val + 1e-6)
-    return im.astype(np.float32) 
+    return im
 
 
-class GatedAttentionPooler(nn.Module):
-    def __init__(
-        self, dim: int, int_dim: int = 512, num_heads: int = 1, out_dim: int = None
-    ):
-        super().__init__()
-
-        self.num_heads = num_heads
-
-        self.attention_v = nn.Sequential(
-            nn.Dropout(0.1), nn.Linear(dim, int_dim), nn.Tanh()
-        )
-        self.attention_u = nn.Sequential(
-            nn.Dropout(0.1), nn.Linear(dim, int_dim), nn.GELU()
-        )
-        self.attention = nn.Linear(int_dim, num_heads)
-
-        self.softmax = nn.Softmax(dim=-1)
-
-        if out_dim is None:
-            self.out_dim = dim * num_heads
-            self.out_proj = nn.Identity()
-        else:
-            self.out_dim = out_dim
-            self.out_proj = nn.Linear(dim * num_heads, out_dim)
-
-    def forward(self, x: torch.Tensor) -> Tuple[Tensor, Tensor]:
-        v = self.attention_v(x)
-        u = self.attention_u(x)
-
-        attn = self.attention(v * u).permute(0, 2, 1)
-        attn = self.softmax(attn)
-
-        x = torch.bmm(attn, x)
-        x = x.view(x.shape[0], -1)
-
-        x = self.out_proj(x)
-        return x, attn
+def save_attention_map(attn, input_shape, output_path):
+    attn = F.interpolate(attn, size=input_shape, mode="bilinear", align_corners=False)
+    attn = make_grid(
+        attn.permute(1, 0, 2, 3),
+        normalize=True,
+        nrow=attn.shape[1],
+        padding=0,
+        scale_each=True,
+    )
+    attn = (attn.permute(1, 2, 0).cpu().numpy() * 255).astype(np.uint8)
+    attn = 255 - attn
+    imsave(output_path + "_attention_map.png", attn)
 
 
-@dataclass
-class ViTPoolModelOutput(ModelOutput):
-    hidden_states: Optional[Tuple[torch.FloatTensor]] = None
-    attentions: Tuple[torch.FloatTensor] = None
-    last_hidden_state: torch.FloatTensor = None
-
-    pool_op: torch.FloatTensor = None
-    pool_attn: torch.FloatTensor = None
-
-    probabilties: torch.FloatTensor = None
-
-
-class ViTPoolClassifier(nn.Module):
-    def __init__(self, config: Dict):
-        super(ViTPoolClassifier, self).__init__()
-
-        self.vit_config = ViTConfig(**config["vit_model"])
-
-        self.encoder = ViTModel(self.vit_config, add_pooling_layer=False)
-
-        pool_config = config.get("pool_model")
-        self.pool_model = GatedAttentionPooler(**pool_config) if pool_config else None
-
-        self.classifier = nn.Sequential(
-            nn.Linear(self.pool_model.out_dim, 512),
-            nn.ReLU(inplace=True),
-            nn.Dropout(p=0.5),
-            nn.Linear(512, 256),
-            nn.ReLU(inplace=True),
-            nn.Dropout(p=0.5),
-            nn.Linear(256, config["num_classes"]),
-        )
-        self.sigmoid = nn.Sigmoid()
-
-    def forward(self, x):
-        b, c, h, w = x.shape
-        outputs = self.encoder(x, output_attentions=True, interpolate_pos_encoding=True)
-
-        if self.pool_model:
-            pool_op, pool_attn = self.pool_model(outputs.last_hidden_state)
-        else:
-            pool_op = torch.mean(outputs.last_hidden_state, dim=1)
-            pool_attn = None
-
-        probs = self.sigmoid(self.classifier(pool_op))
-
-        h_feat = h // self.vit_config.patch_size
-        w_feat = w // self.vit_config.patch_size
-
-        attentions = outputs.attentions[-1][:, :, 0, 1:].reshape(
-            b, self.vit_config.num_attention_heads, h_feat, w_feat
-        )
-
-        pool_attn = pool_attn[:, :, 1:].reshape(
-            b, self.pool_model.num_heads, h_feat, w_feat
-        )
-
-        return ViTPoolModelOutput(
-            hidden_states=outputs.hidden_states,
-            last_hidden_state=outputs.last_hidden_state,
-            attentions=attentions,
-            pool_op=pool_op,
-            pool_attn=pool_attn,
-            probabilties=probs,
-        )
-
-    def load_model_dict(
-        self,
-        encoder_path: str,
-        classifier_path: str,
-        device="cpu",
-    ):
-        checkpoint = torch.load(encoder_path, map_location=device)
-        encoder_ckpt = {
-            k[len("encoder.") :]: v for k, v in checkpoint.items() if "encoder." in k
-        }
-
-        status = self.encoder.load_state_dict(encoder_ckpt)
-        print(f"Encoder status: {status}")
-
-        pool_ckpt = {
-            k.replace("pool_model.", ""): v
-            for k, v in checkpoint.items()
-            if "pool_model." in k
-        }
-        if pool_ckpt:
-            status = self.pool_model.load_state_dict(pool_ckpt)
-            print(f"Pool model status: {status}")
-
-        classifier_ckpt = torch.load(classifier_path, map_location=device)
-        status = self.classifier.load_state_dict(classifier_ckpt)
-        print(f"Classifier status: {status}")
-
-
-def run_model(model, cell_crop, output_path):
+@torch.no_grad()
+def run_model(model, cell_crop, device, output_path):
     cell_crop = np.stack(cell_crop, axis=1)
+    cell_crop = torch.from_numpy(cell_crop).unsqueeze(0).float().to(device)
     cell_crop = min_max_standardize(cell_crop)
-    cell_crop = torch.tensor(cell_crop).float()
+
     output = model(cell_crop)
-    probabilities = output.probabilties.detach()[0]
-    embedding = output.pool_op.detach()[0]
+
+    probabilities = output.probabilties[0]
+    embedding = output.pool_op[0]
     np.save(output_path + "_embedding.npy", embedding)
     np.save(output_path + "_probabilities.npy", probabilities)
-    imsave(output_path + "_attention_map.png", np.int8(output.pool_attn.detach()[0] * 255))
-
+    save_attention_map(
+        output.pool_attn, (cell_crop.shape[2], cell_crop.shape[3]), output_path
+    )
     return np.array(embedding), np.array(probabilities)
--- process.py
+++ process.py
@@ -3,24 +3,34 @@ import datetime
 import logging
 import os
 import sys
-import pandas as pd
 
+os.environ["DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+
+
+import pandas as pd
 import requests
 import torch
-from skimage.io import imread
 import yaml
+from skimage.io import imread
 
 import inference
-
+from vit_model import ViTPoolClassifier
 
 # This is the log configuration. It will log everything to a file AND the console
-logging.basicConfig(filename='log.txt', encoding='utf-8', format='%(levelname)s: %(message)s', filemode='w', level=logging.INFO)
+logging.basicConfig(
+    filename="log.txt",
+    encoding="utf-8",
+    format="%(levelname)s: %(message)s",
+    filemode="w",
+    level=logging.INFO,
+)
 console = logging.StreamHandler()
 logging.getLogger().addHandler(console)
 logger = logging.getLogger("SubCell inference")
 
 # This is the general configuration variable. We are going to use the special key "log" in the dictionary to use the log in our code
-config = { "log": logger}
+config = {"log": logger}
 
 # If you want to use constants with your script, add them here
 config["model_channels"] = "rybg"
@@ -30,11 +40,45 @@ config["create_csv"] = False
 
 # If you want to use command line parameters with your script, add them here
 if len(sys.argv) > 1:
-    argparser = argparse.ArgumentParser(description="Please input the following parameters")
-    argparser.add_argument("-c", "--model_channels", help="channel images to be used [rybg, rbg, ybg, bg]", default="rybg", type=str)
-    argparser.add_argument("-t", "--model_type", help="model type to be used [mae_contrast_supcon_model, vit_supcon_model]", default="mae_contrast_supcon_model", type=str)
-    argparser.add_argument("-u", "--update_model", help="if you want to update the selected model files [True, False]", default=False, type=bool)
-    argparser.add_argument("-csv", "--create_csv", help="if you want to merge the resulting probabilities and features in csv format [True, False]", default=False, type=bool)
+    argparser = argparse.ArgumentParser(
+        description="Please input the following parameters"
+    )
+    argparser.add_argument(
+        "-c",
+        "--model_channels",
+        help="channel images to be used [rybg, rbg, ybg, bg]",
+        default="rybg",
+        type=str,
+    )
+    argparser.add_argument(
+        "-t",
+        "--model_type",
+        help="model type to be used [mae_contrast_supcon_model, vit_supcon_model]",
+        default="mae_contrast_supcon_model",
+        type=str,
+    )
+    argparser.add_argument(
+        "-u",
+        "--update_model",
+        help="if you want to update the selected model files [True, False]",
+        default=False,
+        type=bool,
+    )
+    argparser.add_argument(
+        "-csv",
+        "--create_csv",
+        help="if you want to merge the resulting probabilities and features in csv format [True, False]",
+        default=False,
+        type=bool,
+    )
+    argparse.add_argument(
+        "-g",
+        "--gpu",
+        help="the GPU id to use [0, 1, 2, 3]",
+        default=0,
+        type=int,
+    )
+
     args = argparser.parse_args()
     config = config | args.__dict__
 
@@ -45,48 +89,97 @@ with open("config.yaml", "r") as file:
         config = config | config_contents
 
 # Log the start time and the final configuration so you can keep track of what you did
-config["log"].info('Start: ' + datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
-config["log"].info('Parameters used:')
+config["log"].info("Start: " + datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
+config["log"].info("Parameters used:")
 config["log"].info(config)
-config["log"].info('----------')
+config["log"].info("----------")
 
 
 try:
-    classifier_path = os.path.join("models", config["model_channels"], config["model_type"], "classifier.pth")
-    encoder_path = os.path.join("models", config["model_channels"], config["model_type"], "encoder.pth")
+    classifier_path = os.path.join(
+        "models", config["model_channels"], config["model_type"], "classifier.pth"
+    )
+    encoder_path = os.path.join(
+        "models", config["model_channels"], config["model_type"], "encoder.pth"
+    )
     # Checking for model update
-    if not os.path.isfile(classifier_path) or not os.path.isfile(encoder_path) or config["update_model"]:
+    if (
+        not os.path.isfile(classifier_path)
+        or not os.path.isfile(encoder_path)
+        or config["update_model"]
+    ):
         config["log"].info("- Downloading models...")
-        with open('models_urls.yaml', 'r') as urls_file:
+        with open("models_urls.yaml", "r") as urls_file:
             url_info = yaml.safe_load(urls_file)
 
-            response = requests.get(url_info[config["model_channels"]][config["model_type"]]["classifier"])
+            response = requests.get(
+                url_info[config["model_channels"]][config["model_type"]]["classifier"]
+            )
             if response.status_code == 200:
-                with open(os.path.join("models", config["model_channels"], config["model_type"], "classifier.pth"), 'wb') as downloaded_file:
+                with open(
+                    os.path.join(
+                        "models",
+                        config["model_channels"],
+                        config["model_type"],
+                        "classifier.pth",
+                    ),
+                    "wb",
+                ) as downloaded_file:
                     downloaded_file.write(response.content)
                 config["log"].info("  - classifier.pth updated.")
             else:
                 config["log"].warning("  - classifier.pth url not found.")
 
-            response = requests.get(url_info[config["model_channels"]][config["model_type"]]["encoder"])
+            response = requests.get(
+                url_info[config["model_channels"]][config["model_type"]]["encoder"]
+            )
             if response.status_code == 200:
-                with open(os.path.join("models", config["model_channels"], config["model_type"], "encoder.pth"), 'wb') as downloaded_file:
+                with open(
+                    os.path.join(
+                        "models",
+                        config["model_channels"],
+                        config["model_type"],
+                        "encoder.pth",
+                    ),
+                    "wb",
+                ) as downloaded_file:
                     downloaded_file.write(response.content)
                 config["log"].info("  - encoder.pth updated.")
             else:
                 config["log"].warning("  - encoder.pth url not found.")
 
     # We load the selected model information
-    with open(os.path.join("models", config["model_channels"], config["model_type"], "model_config.yaml"), "r") as config_buffer:
+    with open(
+        os.path.join(
+            "models",
+            config["model_channels"],
+            config["model_type"],
+            "model_config.yaml",
+        ),
+        "r",
+    ) as config_buffer:
         model_config_file = yaml.safe_load(config_buffer)
     model_config = model_config_file.get("model_config")
-    model = inference.ViTPoolClassifier(model_config)
+    model = ViTPoolClassifier(model_config)
     model.load_model_dict(encoder_path, classifier_path)
     model.eval()
 
+    if torch.cuda.is_available():
+        device = torch.device("cuda:" + str(config["gpu"]))
+    else:
+        config["log"].warning("CUDA not available. Using CPU.")
+        device = torch.device("cpu")
+    model.to(device)
+
     # if we want to generate a csv result
     if config["create_csv"]:
-        final_columns = ["id", "top_class_name", "top_class", "top_3_classes_names", "top_3_classes"]
+        final_columns = [
+            "id",
+            "top_class_name",
+            "top_class",
+            "top_3_classes_names",
+            "top_3_classes",
+        ]
         prob_columns = []
         for i in range(31):
             prob_columns.append("prob" + "%02d" % (i,))
@@ -99,7 +192,7 @@ try:
 
     # We iterate over each set of images to process
     if os.path.exists("./path_list.csv"):
-        path_list = open("./path_list.csv", 'r')
+        path_list = open("./path_list.csv", "r")
         for curr_set in path_list:
 
             if curr_set.strip() != "" and not curr_set.startswith("#"):
@@ -118,16 +211,26 @@ try:
                     cell_data.append([imread(curr_set_arr[3].strip(), as_gray=True)])
 
                 # We run the model in inference
-                with torch.no_grad():
-                    embedding, probabilities = inference.run_model(model, cell_data, os.path.join(curr_set_arr[4], curr_set_arr[5].strip()))
+                embedding, probabilities = inference.run_model(
+                    model,
+                    cell_data,
+                    device,
+                    os.path.join(curr_set_arr[4], curr_set_arr[5].strip()),
+                )
 
                 curr_probs_l = probabilities.tolist()
                 max_location_class = curr_probs_l.index(max(curr_probs_l))
                 max_location_name = inference.CLASS2NAME[max_location_class]
-                max_3_location_classes = sorted(range(len(curr_probs_l)), key=lambda sub: curr_probs_l[sub])[-3:]
-                max_3_location_names = (inference.CLASS2NAME[max_3_location_classes[2]] + "," +
-                                        inference.CLASS2NAME[max_3_location_classes[1]] + "," +
-                                        inference.CLASS2NAME[max_3_location_classes[0]])
+                max_3_location_classes = sorted(
+                    range(len(curr_probs_l)), key=lambda sub: curr_probs_l[sub]
+                )[-3:]
+                max_3_location_names = (
+                    inference.CLASS2NAME[max_3_location_classes[2]]
+                    + ","
+                    + inference.CLASS2NAME[max_3_location_classes[1]]
+                    + ","
+                    + inference.CLASS2NAME[max_3_location_classes[0]]
+                )
 
                 # Save results in csv format
                 if config["create_csv"]:
@@ -141,12 +244,18 @@ try:
                     new_row.extend(embedding)
                     df.loc[len(df.index)] = new_row
 
-                config["log"].info("- Saved results for " +curr_set_arr[5].strip() + ", locations predicted [" + max_3_location_names + "]")
+                config["log"].info(
+                    "- Saved results for "
+                    + curr_set_arr[5].strip()
+                    + ", locations predicted ["
+                    + max_3_location_names
+                    + "]"
+                )
 
         if config["create_csv"]:
             df.to_csv("result.csv", index=False)
 except Exception as e:
     config["log"].error("- " + str(e))
 
-config["log"].info('----------')
-config["log"].info('End: ' + datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
+config["log"].info("----------")
+config["log"].info("End: " + datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
